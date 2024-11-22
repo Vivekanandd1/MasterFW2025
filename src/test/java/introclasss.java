eeac493119d46a97e0110f7930204f4a48b59620
vivkekanandd1 <vivekanandd1@live.com>
2024-11-22 13:42:08 +0530

    Commit 6: First code of browser invokation

--- src/test/java/introclasss.java
+++ src/test/java/introclasss.java
@@ -1,4 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class introclasss {
-
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());
+		
+	}
 }
