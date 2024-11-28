064b2bfe8ad7a4fcd107bb25915741b20c53e972
vivkekanandd1 <vivekanandd1@live.com>
2024-11-28 09:57:04 +0530

    Commit 11: Login and data fetch out

--- src/test/java/LoginAndLogout.java
+++ src/test/java/LoginAndLogout.java
@@ -1,15 +1,22 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class LoginAndLogout {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
+		String name = "unknows";
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
-		driver.findElement(By.id("inputUsername")).sendKeys("unknows");
+		driver.findElement(By.id("inputUsername")).sendKeys(name);
 		driver.findElement(By.cssSelector("input[name='inputPassword']")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.cssSelector("button[type='submit']")).click();
+		Thread.sleep(1000);
+		System.out.println(driver.findElement(By.tagName("p")).getText());
+		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+		Thread.sleep(2000);
+		driver.quit();
 	}
 
 }
