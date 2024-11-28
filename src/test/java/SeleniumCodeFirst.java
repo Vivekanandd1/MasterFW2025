c8dd758b8da72f8db55fdb11f637a66a84accdec
vivkekanandd1 <vivekanandd1@live.com>
2024-11-28 09:27:42 +0530

    Commit 9:Successful Login code

--- src/test/java/SeleniumCodeFirst.java
+++ src/test/java/SeleniumCodeFirst.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 public class SeleniumCodeFirst {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("unknows");
@@ -17,6 +17,14 @@ public class SeleniumCodeFirst {
 		driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("daryl@yopmail.com");
 		driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("7441188213");
 		driver.findElement(By.className("reset-pwd-btn")).click();
+		System.out.println(driver.findElement(By.className("infoMsg")).getText());
+		driver.findElement(By.className("go-to-login-btn")).click();
+		driver.findElement(By.id("inputUsername")).sendKeys("unknows");
+		driver.findElement(By.cssSelector("input[name='inputPassword']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
+		
+		Thread.sleep(2000);
+		driver.quit();
 		
 	}
 
