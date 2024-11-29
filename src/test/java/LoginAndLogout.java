e418447633192f92162197221ed04d542ab5d051
vivkekanandd1 <vivekanandd1@live.com>
2024-11-29 09:21:18 +0530

    Commit 12: Password Fetchin

--- src/test/java/LoginAndLogout.java
+++ src/test/java/LoginAndLogout.java
@@ -8,17 +8,34 @@ public class LoginAndLogout {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		String name = "unknows";
+		String Password = getPassword(driver);
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys(name);
-		driver.findElement(By.cssSelector("input[name='inputPassword']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("input[name='inputPassword']")).sendKeys(Password);
+		Thread.sleep(1000);
 		driver.findElement(By.cssSelector("button[type='submit']")).click();
 		Thread.sleep(1000);
 		System.out.println(driver.findElement(By.tagName("p")).getText());
 		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("//button[contains(@class,'logout')]")).click();
-		
 		Thread.sleep(2000);
 		driver.quit();
+	}
+	
+	public static String getPassword(WebDriver driver) throws InterruptedException {
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.className("reset-pwd-btn")).click();
+		String PasswordArr = driver.findElement(By.className("infoMsg")).getText();
+		String[] Passworda = PasswordArr.split("'");
+		String[] Passwordb = Passworda[1].split("'");
+		String Password = Passwordb[0];
+		System.out.println(Password);
+		return Password;
+		
+		
 	}
 
 }
