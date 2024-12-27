6cad9cfb4c2edda71ef6c358e3520e8a12ceb332
vivkekanandd1 <vivekanandd1@live.com>
2024-12-27 09:33:49 +0530

    Commit 16: Improved code to why not user text as locator

--- src/test/java/ECommerce.java
+++ src/test/java/ECommerce.java
@@ -1,4 +1,5 @@
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,19 +10,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ECommerce {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		String[] itemsNeeded = { "Brocolli", "Cucumber" };
 		List<WebElement> Products = driver.findElements(By.cssSelector("h4.product-name"));
-		for (int i=0;i<Products.size();i++) {
-			
-			String Name = Products.get(i).getText();
-			
-			if(Name.contains("Cucumber")) {
+		for (int i = 0; i < Products.size(); i++) {
+
+			String[] Name = Products.get(i).getText().split("-");
+			String FormatedName = Name[0].trim();
+			List itemsNeededList = Arrays.asList(itemsNeeded);
+			int j = 0;
+			if (itemsNeededList.contains(FormatedName)) {
+				System.out.println(FormatedName);
+				j++;
 				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-				break;
+				Thread.sleep(5000);
+				if (j == 2) {
+					break;
+				}
+
 			}
 		}
 
