4c4e951b3abba90b4701cee85afa46fb79a79fd9
vivkekanandd1 <vivekanandd1@live.com>
2025-01-02 09:28:07 +0530

    Commit 21: Link in each tab

--- src/test/java/scope.java
+++ src/test/java/scope.java
@@ -1,14 +1,19 @@
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class scope {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
@@ -20,6 +25,18 @@ public class scope {
 		
 		WebElement Column = Footerdriver.findElement(By.xpath("//table//tr/td[1]/ul"));
 		System.out.println(Column.findElements(By.tagName("a")).size());
+		
+		for(int i=0;i<Column.findElements(By.tagName("a")).size();i++) {
+			//Different approach to click on the links
+//			JavascriptExecutor js = (JavascriptExecutor) driver;
+//			js.executeScript("arguments[0].scrollIntoView();", Column.findElements(By.tagName("a")).get(i));
+//			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+//			wait.until(ExpectedConditions.elementToBeClickable(Column.findElements(By.tagName("a")).get(i)));
+			String Click = Keys.chord(Keys.CONTROL,Keys.ENTER);
+			Column.findElements(By.tagName("a")).get(i).sendKeys(Click);
+			
+			}
+		
 	}
-
+      
 }
