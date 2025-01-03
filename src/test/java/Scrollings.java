889cdbaa45a6151a6c0cf67be0f57cc149b93882
vivkekanandd1 <vivekanandd1@live.com>
2025-01-03 09:34:37 +0530

    Commit 24: Printing the total

--- src/test/java/Scrollings.java
+++ src/test/java/Scrollings.java
@@ -1,7 +1,10 @@
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Scrollings {
@@ -16,6 +19,19 @@ public class Scrollings {
 		js.executeScript("window.scrollBy(0,600)");
 		Thread.sleep(3000);
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+		
+		List<WebElement> values=driver.findElements(By.xpath("//div[@class='tableFixHead']//tbody/tr/td[4]"));
+		int sum=0;
+		for (int i=0;i<values.size();i++) {
+			
+			String value = driver.findElements(By.xpath("//div[@class='tableFixHead']//tbody/tr/td[4]")).get(i).getText();
+			int value1 = Integer.parseInt(value);
+			System.out.println("value is "+ value1);
+			sum =sum +value1;
+			
+		}
+		
+		System.out.println("sum is = " + sum);
 	}
 
 }
