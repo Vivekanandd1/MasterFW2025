b89439ac22bb76e57e9ac42eed21e39bbbc610ab
vivkekanandd1 <vivekanandd1@live.com>
2025-01-16 10:00:17 +0530

    Code for Order Placement

--- src/test/java/MainFW/MainClass.java
+++ src/test/java/MainFW/MainClass.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -48,6 +49,15 @@ public class MainClass {
 		js.executeScript("window.scroll(0,400)");
 		wait.until(ExpectedConditions.elementToBeClickable(CheckoutBtn));
 		CheckoutBtn.click();
+		driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys("123");
+		driver.findElement(By.xpath("(//input[@type='text'])[3]")).sendKeys("Bryan Adams");
+		Actions Act = new Actions(driver);
+		Act.sendKeys(driver.findElement(By.xpath("//input[@placeholder='Select Country']")), "india").build().perform();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));	
+			
+		//driver.findElement(By.xpath("//input[@placeholder='Select Country']")).sendKeys("india");
+		driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+		driver.findElement(By.cssSelector(".action__submit")).click();
 	}
 
 }
