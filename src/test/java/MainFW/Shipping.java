83ebdd48c674befd2ea6b64810c84bdd9a41376d
vivkekanandd1 <vivekanandd1@live.com>
2025-01-22 09:58:56 +0530

    Data Filling

---
+++ src/test/java/MainFW/Shipping.java
@@ -0,0 +1,64 @@
+package MainFW;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import AbstractComponents.AbstractComponent;
+
+public class Shipping extends AbstractComponent{
+	WebDriver driver;
+	
+	public Shipping(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "(//input[@type='text'])[2]")
+	WebElement CVVCode;
+	
+	@FindBy(xpath = "(//input[@type='text'])[3]")
+	WebElement CardHolderName;
+	
+	@FindBy(xpath = "//input[@placeholder='Select Country']")
+	WebElement CountryBox;
+	
+	@FindBy(xpath = "(//button[@type='button'])[2]" )
+	WebElement CountryName;
+	
+	@FindBy(css = "a.action__submit" )
+	WebElement SubmitBtn;
+//	@FindBy(css = ".ta-results")
+//	WebElement ResultBox;
+	By ResultBox = By.cssSelector(".ta-results");
+	By Submit = By.cssSelector("a.action__submit");
+	
+	
+	public void ProductVerification(String CVV,String CardHolder,String Country) {
+		ScreenScroll("window.scrollBy(1049, 564)");
+		CVVCode.sendKeys(CVV);
+		CardHolderName.sendKeys(CardHolder);
+		MovetoElement(CountryBox, Country);
+		CountryName.click();
+
+	}
+	
+	public ConfirmationPage SubmitOrder() {
+		ElementToAppear(Submit);
+		ElementToClick(SubmitBtn);
+		SubmitBtn.click();
+		ConfirmationPage CNFRMPage = new ConfirmationPage(driver);
+		return CNFRMPage;
+	}
+	
+
+	
+}
