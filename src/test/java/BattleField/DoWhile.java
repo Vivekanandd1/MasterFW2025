5ad3f996f713529d04e60012e5f4cf4ffb061810
vivkekanandd1 <vivekanandd1@live.com>
2025-02-28 15:58:09 +0530

    Code Enhancement

---
+++ src/test/java/BattleField/DoWhile.java
@@ -0,0 +1,16 @@
+package BattleField;
+
+public class DoWhile {
+	public static void main(String[] args) {
+		int i=0;
+		
+		do { 
+			i++;
+			System.out.println(i);
+			
+		} while (i<10);
+		System.out.println(i<10);
+		System.out.println("Incremented "+ i);
+	}
+
+}
