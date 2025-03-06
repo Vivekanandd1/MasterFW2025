c7f7a3f9e26932a7e79082bbc0705da9c47bdbfe
vivkekanandd1 <vivekanandd1@live.com>
2025-03-06 08:55:05 +0530

    AccessModifiers

---
+++ src/test/java/BattleFieldAbstractClasses/DemoOne.java
@@ -0,0 +1,11 @@
+package BattleFieldAbstractClasses;
+
+public class DemoOne {
+	
+	
+	public String word = "public word";
+	public void TestMethod() {
+		System.out.println("Called from the another package");
+	}
+
+}
