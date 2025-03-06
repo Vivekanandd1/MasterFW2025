8aa711bf7f97815ad2035c8a80d8df462846c811
vivkekanandd1 <vivekanandd1@live.com>
2025-03-06 08:57:33 +0530

    AccessModifiers 1

---
+++ src/test/java/BattleFieldArrays/DemoTwo.java
@@ -0,0 +1,13 @@
+package BattleFieldArrays;
+
+import BattleFieldAbstractClasses.DemoOne;
+
+public class DemoTwo {
+	
+	public static void main(String[] args) {
+		DemoOne ds = new DemoOne();
+		ds.TestMethod();
+		System.out.println(ds.word);
+	}
+
+}
